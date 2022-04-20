db68f099162b808936f22eee632ee31df8f6949e
the51ht <jhkim@the-51.com>
2022-04-20 14:28:09 +0900

    22.04.20 수정

--- kyobo/ink/js/ht_gnb.js
+++ kyobo/ink/js/ht_gnb.js
@@ -4,17 +4,19 @@ $(function(){
         $('.gnb_menu_area').hide();
         $('.gnb_menu a').removeClass('active');
         $('.ht_gnb_box').hide();
+        $('.ht_gnb_cont').removeClass('chk2').removeClass('chk3');
 
 		$(this).parent().parent().find('a').removeClass('active');
         $(this).addClass('active');
 
         var s = $(this).attr('href');
         $(s).show();
+        $(s).find('.ht_gnb_cont').addClass('chk2');
         e.preventDefault();
     });
 
     $('.gnb2dep a').click(function(e){
-        $(this).closest('.ht_gnb_cont').addClass('chk');
+        $('.ht_gnb_cont').removeClass('chk2');
         $('.gnb3dep .gnb_menu_area, .gnb4dep .gnb_menu_area').hide();
         $('.gnb_menu a').removeClass('active');
 
@@ -22,13 +24,16 @@ $(function(){
         if($(this).hasClass('ht_gnb_arrow')) {
             var s = $(this).attr('href');
             $(s).show();
+            $(this).closest('.ht_gnb_cont').addClass('chk3');
         } else {
             alert('링크연결');
+            $(this).closest('.ht_gnb_cont').addClass('chk2');
         }
         e.preventDefault();
     });
     $('.gnb3dep a').click(function(e){
         $('.gnb4dep .gnb_menu_area').hide();
+        $('.ht_gnb_cont').removeClass('chk2').removeClass('chk3');
 		$(this).parent().parent().find('a').removeClass('active');
         $(this).addClass('active');
         if($(this).hasClass('ht_gnb_arrow')) {
